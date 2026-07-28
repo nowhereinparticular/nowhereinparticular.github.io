021d49e030970f5e3828873d23d7fed5b86f42d1
shade <shanecgo@icloud.com>
2026-07-28 11:51:48 +0800

    Add functions to open and close overlay paper

--- notes.js
+++ notes.js
@@ -1,1 +1,22 @@
+const overlay = document.getElementById("overlay");
+const closeBtn = document.querySelector(".close");
 
+// Opens the paper
+function openPaper() {
+    overlay.classList.add("show");
+}
+
+// Closes the paper
+function closePaper() {
+    overlay.classList.remove("show");
+}
+
+// Close when X is clicked
+closeBtn.addEventListener("click", closePaper);
+
+// Close when clicking outside the paper
+overlay.addEventListener("click", function (event) {
+    if (event.target === overlay) {
+        closePaper();
+    }
+});
